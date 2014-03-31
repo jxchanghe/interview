ac4b0680c669e85a791f4f6b594cceabb81846ed
jxchanghe <jy_huanj@163.com>
2014-03-31 08:44:33 +0800

    update file

--- ini_parser/ini_parser.cc
+++ ini_parser/ini_parser.cc
@@ -3,5 +3,112 @@
 
 namespace qh
 {
+	void Trim(std::string& str, const std::string & ChrsToTrim = " \t\n\r", int TrimDir = 0)
+	{
+		size_t startIndex = str.find_first_not_of(ChrsToTrim);
+		if (startIndex == std::string::npos){str.erase(); return;}
+		if (TrimDir < 2) str = str.substr(startIndex, str.size()-startIndex);
+		if (TrimDir != 1) str = str.substr(0, str.find_last_not_of(ChrsToTrim) + 1);
+	}
 
+	INIParser::INIParser()
+	{
+		
+	}
+
+	INIParser::~INIParser()
+	{
+		
+	}
+
+	bool INIParser::Parse(const std::string& ini_file_path)
+	{
+		std::string sFileData("");
+		std::ifstream inFile (ini_file_path.c_str());
+		
+		if (!inFile.is_open())
+		{
+			return false;
+		}
+		
+		std::string str;
+		while(std::getline(inFile, str))
+		{
+			Trim(str);
+			sFileData = sFileData + str  + "\n";
+		}
+		
+		Parse(sFileData.c_str(), sFileData.size());
+		inFile.close();
+		return true;
+	}
+
+	bool INIParser::ParseOneLine(std::string& strLine, std::string& curSection, const std::string& key_value_seperator)
+	{
+		if (strLine.find('[') != std::string::npos)								
+		{		
+			strLine.erase(strLine.begin());											
+			strLine.erase(strLine.find(']'));																																
+			curSection = strLine;
+		}
+		
+		if(strLine.find(key_value_seperator) != std::string::npos)								
+		{					
+			std::string key = strLine.substr(0, strLine.find(key_value_seperator));							
+			std::string value = strLine.substr(strLine.find(key_value_seperator) + key_value_seperator.size());	
+			mapIniData.insert(std::pair<std::string, std::string>(curSection + key, value));
+		}
+
+		return true;
+	}
+
+	bool INIParser::Parse(const char* ini_data, size_t ini_data_len, const std::string& line_seperator , const std::string& key_value_seperator )
+	{
+		size_t lineSeperatorLen = line_seperator.size();
+		std::string strIniData(ini_data, ini_data_len);
+		size_t pos;
+		std::string curSection("");
+
+		//末尾追加一个行分隔符，方便处理
+		strIniData += line_seperator;
+		pos = strIniData.find(line_seperator);
+		while (pos != std::string::npos)
+		{
+			std::string strLine = strIniData.substr(0, pos);
+			strIniData.erase(0, pos + lineSeperatorLen);
+			pos = strIniData.find(line_seperator);
+			
+			Trim(strLine);
+			if (strLine.size() > 0)
+			   ParseOneLine(strLine, curSection, key_value_seperator);
+		}
+	
+		return true;
+	 }
+
+	 const std::string& INIParser::Get(const std::string& key, bool* found)
+	 {
+		 std::map<std::string , std::string >::iterator l_it;; 
+		 l_it=mapIniData.find(key);
+		
+		 if(l_it == mapIniData.end())
+		 {
+			const static std::string nullResult("");
+
+		   if (NULL != found)   
+			    *found = false;
+            return nullResult;
+		 }
+		 else
+		 {
+			if (NULL != found)  
+				*found = true;
+		    return mapIniData[key];
+		 }
+	 }
+
+	 const std::string& INIParser::Get(const std::string& section, const std::string& key, bool* found)
+	 {
+		return Get(section + key, found);
+	 }
 }
--- ini_parser/ini_parser.h
+++ ini_parser/ini_parser.h
@@ -2,6 +2,9 @@
 #define QIHOO_INI_PARSER_H_
 
 #include <string>
+#include <Map>
+#include <iostream>
+#include <fstream>
 
 namespace qh
 {
@@ -10,12 +13,12 @@ namespace qh
     public:
         INIParser();
         ~INIParser();
-
+		
         //! \brief 解析一个磁盘上的INI文件
         //! \param[in] - const std::string & ini_file_path
         //! \return - bool
         bool Parse(const std::string& ini_file_path);
-
+		
         //! \brief 解析一段形如INI格式的内存数据。
         //!   例如：ini_data="a:1||b:2||c:3"
         //!         调用<code>Parse(ini_data, ini_data_len, "||", ":")</code>即可解析出这段数据。
@@ -27,16 +30,20 @@ namespace qh
         //! \param[in] - const std::string & key_value_seperator
         //! \return - bool
         bool Parse(const char* ini_data, size_t ini_data_len, const std::string& line_seperator = "\n", const std::string& key_value_seperator = "=");
-
+		
         //! \brief 从默认section中查找某个key，并返回找到的value。如果找不到，返回一个空串
         //! \param[in] - const std::string & key
         //! \param[in] - bool * found - 输出参数，true表明找到这个key
         //! \return - const std::string& - 返回的具体key对应的value
         const std::string& Get(const std::string& key, bool* found);
-
+		
         const std::string& Get(const std::string& section, const std::string& key, bool* found);
-
+		
     private:
+		bool ParseOneLine(std::string& strLine, std::string& curSection, const std::string& key_value_seperator = "=");
+		 //! key是由ini文件中的key + key所属的section复合而成
+		std::map<std::string, std::string> mapIniData;
+
     };
 }
 
